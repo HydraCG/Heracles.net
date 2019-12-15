0180501c90fca9bb226bc68f502a82b0f8611c58
alien-mcl <alien-mcl@users.noreply.github.com>
2019-12-15 16:46:48 +0100

    Added some nuspec details

--- .build/VersionAssemblyInfo.cs
+++ .build/VersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 [assembly: System.Reflection.AssemblyVersion("0.5.0.0")] 
 [assembly: System.Reflection.AssemblyFileVersion("0.5.0.0")] 
-[assembly: System.Reflection.AssemblyInformationalVersion("0.5.0.0-dd92a05e07dda7a58a246c3d62146e0223cbbc19")] 
+[assembly: System.Reflection.AssemblyInformationalVersion("0.5.0.0")] 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Commit hash added to AssemblyInformationalVersion is on purpose.")] 
